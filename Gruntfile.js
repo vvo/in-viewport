42ba02eb84d00fe81864f959daa2b44804ad975a
vvo <vincent.voyer@gmail.com>
2013-05-18 16:52:37 +0200

    remove notify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-notify');
 
   // Default task.
   grunt.registerTask('default', [
