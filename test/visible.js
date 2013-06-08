686286e7fa6051b0a63d4d8eaa13a4e5e341d1fd
vvo <vincent.voyer@gmail.com>
2013-06-08 18:06:50 +0200

    make visible test async
    While the callback is immediately called because element is immediately
    visible, it must be/could be async

--- test/visible.js
+++ test/visible.js
@@ -2,10 +2,11 @@ describe('asking if a visible div scrolled', function() {
   var scrolled = false;
   var test = createTest();
 
-  before(function() {
+  before(function(done) {
     insertTest(test);
     inViewport(test, function() {
       scrolled = true;
+      done();
     });
   });
 
